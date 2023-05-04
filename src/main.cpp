0fefc071afc68e41efe5e450d725e66212aacea7
Krzysztof Pochwała <k.pochwala@gmail.com>
2023-05-04 22:57:25 +0200

    dev container now based on ros:foxy instead of generic focal ubuntu, added micro ros to platformio

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,25 @@
 #include <ezLED.h>
 #include <Preferences.h>
 
+#include <micro_ros_platformio.h>
+#include <rcl/rcl.h>
+#include <rcl/error_handling.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+#include <geometry_msgs/msg/twist.h>
+
+#if !defined(MICRO_ROS_TRANSPORT_ARDUINO_WIFI)
+#error This example is only avaliable for Arduino framework with wifi transport.
+#endif
+
+rcl_publisher_t publisher;
+rcl_subscription_t subscriber;
+geometry_msgs__msg__Twist msg;
+
+rclc_executor_t executor;
+rclc_support_t support;
+rcl_allocator_t allocator;
+rcl_node_t node;
 
 // static constexpr auto pin = 40;
 static constexpr auto left_pin = 15;
